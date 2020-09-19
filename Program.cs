fc4e0c6feacfaa9a852a3a1498fed5fd0855e5a7
KellyCristinaAires <k.woys@hotmail.com>
2020-09-18 23:44:41 -0300

    atividade nome completo

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace nomecompeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string sobrenome;
+            Console.Write("\n");
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Digite seu sobrenome: ");
+            sobrenome =Console.ReadLine();
+            Console.Write("\n");
+            Console.Write($"nome completo: {nome} {sobrenome}");
+            Console.Write("\n");
+            Console.WriteLine($"nome de catalogo: {sobrenome.ToUpper()}, {nome}");
+            Console.Write("\n");
+            Console.WriteLine("pressione enter para sair");
+            Console.ReadKey();
+        }
+    }
+}
+
